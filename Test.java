aad875561be0f20c0da9b92344fa4f308e912608
JaguMadhura <2420030535@klh.edu.in>
2025-07-26 11:37:34 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package Madhura;
+
+public class Test {
+	public static void main(String[] args) {
+		int a = 59;
+		int b = 79;
+		int c = 556;
+		c =  a ^ b;
+		System.out.println("a^b="+c);
+		
+		c = ~a;
+		System.out.println("~a="+c);
+	}
+
+}
