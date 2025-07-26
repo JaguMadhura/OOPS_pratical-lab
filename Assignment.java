c1692a674c2dd5dd6c05b60822ca437f60483fde
JaguMadhura <2420030535@klh.edu.in>
2025-07-26 11:35:44 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,18 @@
+package Madhura;
+
+public class Assignment {
+	public static void main(String[] args) {
+		int a = 8;
+		int var;
+		
+		var = a;
+		System.out.println("var using =: "+ var);
+		
+		var += a;
+		System.out.println("var using +=: "+ var);
+		
+		var *= a;
+		System.out.println("var using *=: "+ var);
+	}
+
+}
