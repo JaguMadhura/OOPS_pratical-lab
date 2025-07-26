7b62e4baa3a7a01a21448d1c5cd7967163f4ad24
JaguMadhura <2420030535@klh.edu.in>
2025-07-26 11:35:32 +0530

    Add files via upload

---
+++ Arthmatic_Operations.java
@@ -0,0 +1,22 @@
+package Madhura;
+
+public class Arthmatic_Operations {
+
+	
+		public static void main(String[] args) {
+		int a = 10, b = 15;
+		System.out.println("a+b="+ (a+b));
+		
+		System.out.println("a-b="+(a-b));
+		
+		System.out.println("a*b="+(a*b));
+		
+		System.out.println("a/b="+(a/b));
+		
+		System.out.println("a%b="+(a%b));
+		
+
+	}
+	}
+
+
