78f16f9a2d99845b30dd7c66452a8e7a7e5bd2b5
JaguMadhura <2420030535@klh.edu.in>
2025-07-26 11:37:48 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,19 @@
+package Madhura;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		int a= 45;
+		int c = 60;
+		
+		c = a<<2;
+		System.out.println("a<<2="+c);
+		
+		c = a>>2;
+		System.out.println("a>>2="+c);
+		
+		c = a>>>2;
+		System.out.println("a>>>2="+c);
+	}
+	
+}
