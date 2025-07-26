0aba03c6d346a81e0ebf3375f5460b3150e34d4c
JaguMadhura <2420030535@klh.edu.in>
2025-07-26 11:37:09 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,13 @@
+package Madhura;
+
+public class Ternary {
+	public static void main(String[] args) {
+		int februaryDays = 29;
+		String result;
+		
+		result = (februaryDays == 28) ? "Not a leap year" : "Leap year";
+		System.out.println(result);
+		
+	}
+
+}
