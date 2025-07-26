60fedc5f58ba23d897ffd75229565e12a22e50e3
JaguMadhura <2420030535@klh.edu.in>
2025-07-26 11:35:55 +0530

    Add files via upload

---
+++ Bitwise_operator.java
@@ -0,0 +1,18 @@
+package Madhura;
+
+public class Bitwise_operator {
+	public static void main(String[] args) {
+		int a = 70;
+		int b = 80;
+		int c = 20;
+		
+		c = a & b;
+		System.out.println("a&b="+c);
+		
+		c=a|b;
+		System.out.println("a|b="+c);
+		
+	}
+	
+
+}
