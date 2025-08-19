b55b2840b416ce21ab923172f31740023c73a61c
JaguMadhura <2420030535@klh.edu.in>
2025-08-19 14:32:05 +0530

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,24 @@
+package Madhura;
+
+public class Encapsulation {
+	
+	    static class Programmer {
+	        private String name;
+
+	        public String getName() {
+	            return name;
+	        }
+
+	        public void setName(String name) {
+	            this.name = name;
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        Programmer p = new Programmer();
+	        p.setName("Geek");
+	        System.out.println("Name=> " + p.getName());
+	    }
+	}
+
+
